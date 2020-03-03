3ea7fd46b875de542c69ff5684d0c733d4469928
jinsyin <jinsyin@gmail.com>
2020-03-03 17:03:59 +0800

    Remove unnecessary files

--- _scripts/cibuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-# Travis will automatically install the dependencies based on the referenced gems (Gemfile).
-bundle exec jekyll build        # build
-#bundle exec htmlproofer ./_site # check
